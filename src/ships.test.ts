da0913603e6acc4fe59ddce68512ebfbf456c175
Mitchell Mudd <mitchellmudd@outlook.com>
2022-07-08 22:08:55 -0400

    Switched to an interface from just an object. Found the value in testing when I had to refactor!

--- src/ships.test.ts
+++ src/ships.test.ts
@@ -2,10 +2,12 @@ const myShipFunctions = require('./ships.ts')
 
 describe("Testing to see if creating a ship outputs correctly and incorrectly", () => {
     test('Creating a ship works', () => {
-        expect(myShipFunctions.createShip(3, [10, 11, 12])).toEqual({
+        let testShip = {
             "length": 3,
-            "coords": [10, 11, 12]
-        });
+            "coords": [10, 11, 12],
+            "hit": expect.any(Function)
+        }
+        expect(myShipFunctions.createShip(3, [10, 11, 12])).toMatchObject(testShip);
     });
 
     test('coords array.length matches that of the length property', () => {
@@ -13,3 +15,17 @@ describe("Testing to see if creating a ship outputs correctly and incorrectly", 
         expect(ship.length).toBe(ship.coords.length);
     })
 });
+
+describe("Testing the ship methods", () => {
+    test('Hit method changes the value of the hit coordinate', () => {
+        const ship = myShipFunctions.createShip(3, [1, 2, 3]);
+        ship.hit(1)
+        expect(ship.coords).toEqual([0, 2, 3]);
+    });
+
+    test('Hit method changes the value of the hit coordinate #2', () => {
+        const ship = myShipFunctions.createShip(3, [1, 2, 3]);
+        ship.hit(3)
+        expect(ship.coords).toEqual([1, 2, 0]);
+    })
+})
--- src/ships.ts
+++ src/ships.ts
@@ -1,28 +1,26 @@
-let shipActions = {
-    hit(position: number, isSunkCallBack: Function) {
-    for (let coord of this.coords) {
-        if (position === coord) {
-            // 0 means it's been hit
-            coord = 0;
+function createShip(length: number, coords: number[]): battleship {
+    let ship: battleship = {
+        length: length,
+        coords: coords,
+        hit: (position: number) => {
+            console.log(this)
+            if (coords.indexOf(position) !== -1) {
+                let hitPosition = coords.indexOf(position);
+                coords[hitPosition] = 0;
+            }
         }
-    }     
-    isSunkCallBack(this.coords)      
-    },
-    isSunk(coords: number[]) {
-    for (let coord of coords) {
-        if (coord != 0) {
-            return;
-        }
-        return true
     }
-    }
-}
-
-function createShip(length: number, coords?: number[]) {
-    let ship = Object.create(shipActions);
-    ship.length = length;
-    ship.coords = coords;
     return ship;
 };
 
+interface battleship {
+    length: number,
+    coords: number[],
+    hit: (position: number) => void
+}
+
 module.exports.createShip = createShip;
+
+const testShip = createShip(3, [1, 2, 3]);
+testShip.hit(1)
+
